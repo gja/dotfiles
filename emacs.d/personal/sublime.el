a5eed90751361fac4a58ee45772fe19454449660
Tejas Dinkar <tejas@gja.in>
2013-10-26 15:55:59 +0530

    Duplicate line

--- emacs.d/personal/sublime.el
+++ emacs.d/personal/sublime.el
@@ -1,6 +1,16 @@
+(defun duplicate-line ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
+
 (global-set-key (kbd "M-t") 'projectile-find-file)
 (global-set-key (kbd "M-s-<right>") 'switch-to-next-buffer)
 (global-set-key (kbd "M-s-<left>") 'switch-to-prev-buffer)
+(global-set-key (kbd "M-D") 'duplicate-line)
 
 (eval-after-load 'rspec-mode
   '(progn
