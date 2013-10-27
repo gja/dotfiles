e6dfecb30d0bc2eeed23e45efbc271c3c8cdc590
Tejas Dinkar <tejas@gja.in>
2013-10-27 07:59:32 +0530

    Mastered the art of emacs macro writing

--- emacs.d/personal/sublime.el
+++ emacs.d/personal/sublime.el
@@ -1,21 +1,26 @@
-(defun duplicate-line ()
-  (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (open-line 1)
-  (forward-line 1)
-  (yank))
+(defmacro move-back-horizontal-after (&rest code)
+  `(let ((horizontal-position (current-column)))
+     (progn
+       ,@code
+       (move-to-column horizontal-position))))
 
 (defun comment-or-uncomment-line-or-region ()
   (interactive)
   (if (region-active-p)
     (comment-or-uncomment-region (region-beginning) (region-end))
-    (let ((horizontal-position (current-column)))
-      (progn
-        (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-        (forward-line 1)
-        (move-to-column horizontal-position)))))
+    (move-back-horizontal-after
+     (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+     (forward-line 1))))
+
+(defun duplicate-line ()
+  (interactive)
+  (move-back-horizontal-after
+   (move-beginning-of-line 1)
+   (kill-line)
+   (yank)
+   (open-line 1)
+   (forward-line 1)
+   (yank)))
 
 (global-set-key (kbd "M-t") 'projectile-find-file)
 (global-set-key (kbd "M-s-<right>") 'switch-to-next-buffer)
@@ -25,8 +30,8 @@
 
 (eval-after-load 'rspec-mode
   '(progn
-    (define-key rspec-mode-map (kbd "M-R") 'rspec-verify-single)
-    (define-key rspec-mode-map (kbd "M-T") 'rspec-verify)
-    (define-key ruby-mode-map (kbd "M-E") 'rspec-rerun)
-    (define-key ruby-mode-map (kbd "M-s-<down>") 'rspec-toggle-spec-and-target)
-    (rspec-install-snippets)))
+     (define-key rspec-mode-map (kbd "M-R") 'rspec-verify-single)
+     (define-key rspec-mode-map (kbd "M-T") 'rspec-verify)
+     (define-key ruby-mode-map (kbd "M-E") 'rspec-rerun)
+     (define-key ruby-mode-map (kbd "M-s-<down>") 'rspec-toggle-spec-and-target)
+     (rspec-install-snippets)))
