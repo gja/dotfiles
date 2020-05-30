af4e6da39865df7506da41d4f3d3d0807bd30348
Tejas Dinkar <tejas@gja.in>
2020-05-30 19:21:12 +0530

    Adding homebrew scripts

---
+++ homebrew-script.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+## Assuming Homebrew is already installed
+
+brew install golang nodejs
+
+brew install bash bash-completion mas
+brew cask install zoomus iterm2 visual-studio-code superhuman slack brave-browser docker whatsapp pastebot turbo-boost-switcher
+mas lucky Todoist
+mas lucky Divvy
