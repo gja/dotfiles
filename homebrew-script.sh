aa7f3bc195ba3b750bbdf8886271f3424d2d47e6
Tejas Dinkar <tejas@gja.in>
2021-01-25 07:43:35 +0530

    Foo

--- homebrew-script.sh
+++ homebrew-script.sh
@@ -3,7 +3,7 @@
 ## Assuming Homebrew is already installed
 
 brew install bash bash-completion mas
-brew cask install zoomus iterm2 visual-studio-code superhuman slack brave-browser docker whatsapp turbo-boost-switcher time-out
+brew cask install zoomus iterm2 visual-studio-code superhuman slack brave-browser docker whatsapp turbo-boost-switcher time-out nordvpn
 
 mas lucky Todoist
 mas lucky Divvy
